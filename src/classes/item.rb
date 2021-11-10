3536d1e8d8d59b73b9776a73c46b56b093b5541d
Erick Contreras <eri89--@hotmail.com>
2021-11-10 11:12:05 -0600

    Add all methods

--- src/classes/item.rb
+++ src/classes/item.rb
@@ -6,4 +6,24 @@ class Item
     @publish_date = publish_date
     @archived = archived
   end
+
+  def add_genre(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
+  end
+
+  def add_author(author)
+    @author = author
+    author.items << self unless author.items.include?(self)
+  end
+
+  def add_source(source)
+    @source = source
+    source.items << self unless source.items.include?(self)
+  end
+
+  def add_label(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
+  end
 end
