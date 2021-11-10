fabc223651064993386cc94803af2c5dbf14fc08
Erick Contreras <eri89--@hotmail.com>
2021-11-10 11:07:23 -0600

    Add item class properties

--- src/classes/item.rb
+++ src/classes/item.rb
@@ -1,3 +1,9 @@
 class Item
-  def initialize; end
+  attr_reader :genre, :author, :label, :source
+
+  def initialize(publish_date, id = Random.rand(1..10_000), archived: false)
+    @id = id
+    @publish_date = publish_date
+    @archived = archived
+  end
 end
